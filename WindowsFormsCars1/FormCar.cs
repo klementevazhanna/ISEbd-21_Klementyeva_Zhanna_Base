9fc4158406b7c15742e9f13be09480bd8af8e48d
klementevazhanna <klementoss44@gmail.com>
2021-10-24 15:31:46 +0400

    Lab1

---
+++ WindowsFormsCars1/FormCar.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsCars1
+
+{
+    public partial class FormCar : Form
+    {
+        private SportCar car;
+        public FormCar()
+        {
+            InitializeComponent();
+        }
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            car.DrawTransport(gr);
+            pictureBox1.Image = bmp;
+        }
+
+        private void buttonСreate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            car = new SportCar();
+            car.Init(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue,
+           Color.Yellow, true, true); car.SetPosition(rnd.Next(20, 100),
+           rnd.Next(50, 200), pictureBox1.Width, pictureBox1.Height);
+            //pictureBox1.Height
+            Draw();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            //получаем имя кнопки
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp1":
+                    car.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown1":
+                    car.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft1":
+                    car.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight1":
+                   car.MoveTransport(Direction.Right);
+                    break;
+                case "pictureBox1_Click":
+                    
+                    break;
+
+            }
+            Draw();
+        }
+   
+
+        private void FormCar_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+} 
