6dd732ea7a2b3ca1ca40f295d7eaf2d15704a031
Savitskiy2002 <95118902+Savitskiy2002@users.noreply.github.com>
2021-12-10 21:33:12 +0400

    Lab6

--- WindowsFormsCars1/GavanForm.cs
+++ WindowsFormsCars1/GavanForm.cs
@@ -150,5 +150,37 @@ namespace WindowsFormsCars1
             formCarConfig.AddEvent(AddCar);
             formCarConfig.Show();
         }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (parkingColl.SaveData(saveFileDialog.FileName))
+                {
+                    MessageBox.Show("Сохранение прошло успешно", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Не сохранилось", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (parkingColl.LoadData(openFileDialog.FileName))
+                {
+                    MessageBox.Show("Загрузили", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ReloadLevels();
+                    Draw();
+                }
+                else
+                {
+                    MessageBox.Show("Не загрузили", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
     }
 }
--- WindowsFormsCars1/ParkingCollection.cs
+++ WindowsFormsCars1/ParkingCollection.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace WindowsFormsCars1
 {
@@ -11,6 +10,7 @@ namespace WindowsFormsCars1
         readonly Dictionary<string, Parking<ITransport>> parkingStages;
         private readonly int pictureWidth;
         private readonly int pictureHeight;
+        private readonly char separator = ':';
 
         public List<string> Keys => parkingStages.Keys.ToList();
 
@@ -41,6 +41,92 @@ namespace WindowsFormsCars1
                 }
                 return null;
             }
+        }
+
+        public bool SaveData(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+            using (StreamWriter sw = new StreamWriter(filename, false))
+            {
+                sw.Write($"ParkingCollection{Environment.NewLine}");
+                foreach (var level in parkingStages)
+                {
+                    //Начинаем парковку 
+                    sw.Write($"Parking{separator}{level.Key}{Environment.NewLine}");
+                    ITransport car = null;
+                    for (int i = 0; (car = level.Value.GetNext(i)) != null; i++)
+                    {
+                        if (car != null)
+                        {
+                            //если место не пустое 
+                            //Записываем тип машины 
+                            if (car.GetType().Name == "BaseBoat")
+                            {
+                                sw.Write($"BaseBoat{separator}");
+                            }
+                            if (car.GetType().Name == "Cater")
+                            {
+                                sw.Write($"Cater{separator}");
+                            }
+                            //Записываемые параметры 
+                            sw.Write(car + Environment.NewLine);
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool LoadData(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                return false;
+            }
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                string strs = sr.ReadLine();
+                if (strs.Contains("ParkingCollection"))
+                {
+                    //очищаем записи 
+                    parkingStages.Clear();
+                }
+                else
+                {
+                    //если нет такой записи, то это не те данные 
+                    return false;
+                }
+                AbstrBoat car = null;
+                string key = string.Empty;
+                while ((strs = sr.ReadLine()) != null)
+                {
+                    if (strs.Contains("Parking"))
+                    {
+                        //начинаем новую парковку 
+                        key = strs.Split(separator)[1];
+                        parkingStages.Add(key, new Parking<ITransport>(pictureWidth, pictureHeight));
+                    }
+                    else if (strs.Contains(separator))
+                    {
+                        if (strs.Contains("BaseBoat"))
+                        {
+                            car = new BaseBoat(strs.Split(separator)[1]);
+                        }
+                        else if (strs.Contains("Cater"))
+                        {
+                            car = new Cater(strs.Split(separator)[1]);
+                        }
+                        if (!(parkingStages[key] + car))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
         }
     }
 }
